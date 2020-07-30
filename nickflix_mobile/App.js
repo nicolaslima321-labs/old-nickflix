a6205e2fd3bfbfb7b63c5adef8c51f2bc8b81bef
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-07-30 17:48:14 -0300

    Adding Bebas Neue Font

--- nickflix_mobile/App.js
+++ nickflix_mobile/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "BebasNeue-Regular" }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
