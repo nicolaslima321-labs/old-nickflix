40f8fb381072852fe85333ece645e2e513681685
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-08-10 16:15:51 -0300

    Dashboard initialized

---
+++ nickflix-frontend/src/pages/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+
+// import axios from 'axios'
+
+const API = 'http://api.nickflix:8000/api'
+
+export default {
+  name: 'Dashboard',
+
+  data () {
+    return {
+      
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
