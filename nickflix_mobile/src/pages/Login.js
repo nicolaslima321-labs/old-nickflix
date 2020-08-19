a0e4f932510ec2b0840ef4f3958ef1c55c32f787
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-08-19 20:15:09 -0300

    Start API Consum

--- nickflix_mobile/src/pages/Login.js
+++ nickflix_mobile/src/pages/Login.js
@@ -5,7 +5,31 @@ import { Button, View, Text, TextInput, Image, StatusBar, StyleSheet, TouchableO
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import axios from 'axios'
+
 function Login ({ navigation }) {
+  const [login, setLogin] = React.useState(null)
+  const [password, setPassword] = React.useState(null)
+
+  function authenticate() {
+    const API = "http://api.nickflix:8001/api"
+
+    const data = {
+      email: login,
+      password: password
+    }
+  
+    axios.post(`${API}/auth/login`, data)
+      .then((response) => {
+        console.log("Deu bom", data)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log("Deu ruim", data)
+        console.log(error)
+      })
+  }
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#000', paddingBottom: 50}}>
       <StatusBar
@@ -17,16 +41,18 @@ function Login ({ navigation }) {
         placeholder = "E-mail ou Número de Telefone"
         placeholderTextColor = "grey"
         style={ styles.inputFields }
+        onChangeText={() => setLogin}
       />
       <TextInput
         underlineColorAndroid = "transparent"
         placeholder = "Senha"
         placeholderTextColor = "grey"
         style={ styles.inputFields }
+        onChangeText={() => setPassword}
       />
       <TouchableOpacity
         style={ styles.buttonFields }
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => authenticate()}
       >
         <Text style={{ color: "#fff" }}>Entrar</Text>
       </TouchableOpacity>
