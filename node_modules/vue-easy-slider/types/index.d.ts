53a608475861707ebb798cbbdd26b9f4644d3a3e
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2021-10-13 21:52:17 -0300

    removed node_modules (wtf)

--- node_modules/vue-easy-slider/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import Vue, { PluginObject, PluginFunction } from 'vue'
-
-export class SliderPlugin implements PluginObject<{}> {
-  install: PluginFunction<{}>
-  static install(pVue: typeof Vue, options?: {} | undefined): void
-}
-
-export class Slider extends Vue {}
-export class SliderItem extends Vue {}
